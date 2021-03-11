cea1b7e72a2fde6a00adc17e5f8605683c496347
Randi Reyna <randireyna@live.com>
2021-03-10 21:10:37 -0600

    first commit

---
+++ calculator.py
@@ -0,0 +1,21 @@
+num_1 = int(input("Enter a number"))
+num_2 = int(input("Enter another number"))
+
+print(num_1 + num_2)
+
+
+num_3 = int(input("Enter a Number"))
+num_4 = int(input("Enter another Number"))
+
+print(num_3 * num_4)
+
+num_5 = int(input("Enter a Number"))
+num_6 = int(input("Enter another Number"))
+
+print(num_5 / num_6)
+
+
+num_7 = int(input("Enter a Number"))
+num_8 = int(input("Enter another Number"))
+
+print(num_7 - num_8)
